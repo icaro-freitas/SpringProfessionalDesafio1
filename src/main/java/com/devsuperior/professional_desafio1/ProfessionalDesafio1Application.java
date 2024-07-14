b8cf041289b54ae2b658de25af18bd11a119ab07
icaro-freitas <icaro_freitas2014@outlook.com>
2024-07-14 14:06:01 -0300

    Project created

---
+++ src/main/java/com/devsuperior/professional_desafio1/ProfessionalDesafio1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.professional_desafio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalDesafio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalDesafio1Application.class, args);
+	}
+
+}
